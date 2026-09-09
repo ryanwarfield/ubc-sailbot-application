f3c079d7cc3735b905a1f76e0dc282f0fcb6919c
Ryan Warfield <ryanwarfield99@gmail.com>
2026-09-09 14:16:24 -0700

    feat: add both funciton implementations

--- python/standard_calc.py
+++ python/standard_calc.py
@@ -11,7 +11,16 @@ def bound_to_180(angle):
     Returns:
         float: The bounded angle in degrees.
     """
-    return 0
+
+    reoriented = angle + 180
+    bounded = reoriented % 360 # fortunately for us python supports modulus on floating point numbers
+    oriented_back = bounded - 180
+
+    # -180 is only valid backwards angle
+    if oriented_back == 180:
+        return -180
+    else:
+        return oriented_back
 
 
 def is_angle_between(first_angle, middle_angle, second_angle):
@@ -29,4 +38,18 @@ def is_angle_between(first_angle, middle_angle, second_angle):
     Returns:
         bool: True when `middle_angle` is not in the reflex angle of `first_angle` and `second_angle`, false otherwise.
     """
-    return True
+
+    first_angle_normalized = bound_to_180(first_angle)
+    middle_angle_normalized = bound_to_180(middle_angle)
+    second_angle_normalized = bound_to_180(second_angle)
+
+    smaller_bound = min(first_angle_normalized, second_angle_normalized)
+    bigger_bound = max(first_angle_normalized, second_angle_normalized)
+
+    if bigger_bound - smaller_bound < 180:
+        return smaller_bound < middle_angle_normalized and middle_angle_normalized < bigger_bound
+    elif bigger_bound - smaller_bound > 180:
+        return middle_angle_normalized < smaller_bound or bigger_bound < middle_angle_normalized
+    else:
+        # the bounds are exactly 180 degrees apart, so there is no well defined reflex arc/minor arc
+        return False
